9f1d4b857ea06d8dec2dfa0ee0b43cfbd57892a9
plexavor <tuanxerrorx@gmail.com>
2023-02-17 09:45:09 +0700

    y

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hai, Kami Zenkafako.</span> Kami adalah tim kecil yang beranggota-kan 4 orang yaitu Zahwa Zein, Dika Ardianta, Fatih Arridho, Syiko. Kami bergerak dibidang Web Developer dan Bot Developer. Kami membuat Bot WhatsApp bernama Hisoka Morrou, Bot Telegram bernama Obito Uchiha, dan membuat Rest Full APIs yaitu api.zahwazein.xyz."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
